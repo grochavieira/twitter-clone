befd8d9a457fa04d655a00dac5eeeae81a1bfbad
Guilherme Rocha Vieira <guilherme.rocha@nescara.com>
2023-11-08 09:15:56 -0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -49,3 +49,38 @@ function setLoading(isLoading) {
 
 window.addEventListener("hashchange", router);
 window.addEventListener("load", router);
+
+function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+}
+
+function openToast(data) {
+    if (data.status) {
+        if (data.status === 'success') document.getElementById('toast').style.borderBottomColor = '#52B788';
+        if (data.status === 'warning') document.getElementById('toast').style.borderBottomColor = '#FADF7F';
+        if (data.status === 'error') document.getElementById('toast').style.borderBottomColor = '#E71D36';
+    }
+
+    if (data.title) {
+        document.getElementById('toast-title').innerText = data.title;
+    } else {
+        document.getElementById('toast-title').innerText = '';
+    }
+
+    if (data.message) {
+        document.getElementById('toast-message').innerText = data.message;
+    } else {
+        document.getElementById('toast-message').innerText = '';
+    }
+
+    document.getElementById('toast').style.display = 'initial';
+
+    setTimeout(() => {
+        document.getElementById('toast').style.display = 'none';
+    }, 5000);
+}
+
+function closeToast() {
+    document.getElementById('toast').style.display = 'none';
+}
